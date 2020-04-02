a964ae161df844ed67d814c0f9b1fa36f3b191f9
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-01 19:44:04 -0600

    Add test so admin can add new tutorial and sad path testing

--- spec/features/admin/admin_can_add_content_spec.rb
+++ spec/features/admin/admin_can_add_content_spec.rb
@@ -28,5 +28,38 @@ describe 'Admin can create tutorial' do
 
       expect(page).to have_content(tutorial.title)
     end
+
+    it "I cannot leave title blank to creat tutorial" do
+      visit new_admin_tutorial_path
+
+      fill_in "tutorial_title", with: ""
+      fill_in "tutorial_description", with: "Real deal content right here."
+      fill_in "tutorial_thumbnail", with: "https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg"
+      click_on "Save"
+
+      expect(page).to have_content("Title can't be blank")
+    end
+
+    it "I cannot leave description blank to creat tutorial" do
+      visit new_admin_tutorial_path
+
+      fill_in "tutorial_title", with: "Mod 4"
+      fill_in "tutorial_description", with: ""
+      fill_in "tutorial_thumbnail", with: "https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg"
+      click_on "Save"
+
+      expect(page).to have_content("Description can't be blank")
+    end
+
+    it "I cannot leave thumbnail blank to create tutorial" do
+      visit new_admin_tutorial_path
+
+      fill_in "tutorial_title", with: "Mod 4"
+      fill_in "tutorial_description", with: "Real deal content right here."
+      fill_in "tutorial_thumbnail", with: ""
+      click_on "Save"
+
+      expect(page).to have_content("Thumbnail can't be blank")
+    end
   end
 end
