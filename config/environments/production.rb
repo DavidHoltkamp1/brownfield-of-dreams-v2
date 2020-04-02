124209262bed18107ff174210cd1ef2ef0692817
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-02 12:52:56 -0600

    Update to production env sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,14 +97,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 '587',
-    domain:               'example.com',
-    user_name:            ENV["SENDGRID_USERNAME"],
-    password:             ENV["SENDGRID_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+  :address        => 'smtp.SendGrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+config.action_mailer.default_url_options = { host: "https://brownfield-of-dreams-pair.herokuapp.com" }
   # Do not swallow errors in after_commit/after_rollback callbacks.
   # config.active_record.raise_in_transactional_callbacks = true
 end
